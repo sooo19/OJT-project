0cfcbc6302ea21f0724e1313294c535ad834295b
yong9753 <dldydgns53@naver.com>
2023-01-06 16:58:38 +0900

    feat: 인증적용에 대한 로직 확장 포인트 작성

--- src/main/java/com/example/server/domain/controller/MemberController.java
+++ src/main/java/com/example/server/domain/controller/MemberController.java
@@ -13,13 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final Long internId = 1L;
+    private final Long buddyId = 2L;
 
     @GetMapping("/{memberId}")
     public MemberWithBuddyInfoDto internWithBuddyInfo(@PathVariable Long memberId){
-        if(memberId == 1){
+        if(memberId == internId){
             // 인턴
             return memberService.getInternWithBuddyInfo(memberId);
+        }else if(memberId == buddyId){
+            return memberService.getBuddyWithInternInfo(memberId);
         }
-        return memberService.getBuddyWithInternInfo(memberId);
+        throw new RuntimeException("서비스 확장 Point - 인증 인가 적용 및 Thread Local 활용 필요");
     }
 }
